038bce234afda19f6e4aca98e448163342f06f6d
gusdn <gusdn@DESKTOP-S61ANMI>
2024-01-16 21:44:37 +0900

    숙제끝

--- hgjava/src/todo/Todo0116.java
+++ hgjava/src/todo/Todo0116.java
@@ -6,15 +6,18 @@ public class Todo0116 {
 
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
-		
+		while(true) {
 		System.out.println("값을 입력>>>");
 		String input = scn.nextLine();
 		//input.equals("quit")
-		
 		//입력값은 홍길동입니다.
 		
 		//"quit" 값이 들어오면 <<end of prog>>
-		
+		if(input.equals ("quit")) {
+			break;
+			
+		}
 	}
 
+}
 }
