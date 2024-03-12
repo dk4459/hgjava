43fe0ee249e89e810fdf9bb62bd69a6aed0a5039
User <User@DESKTOP-NARS842>
2024-03-12 20:19:36 +0900

    qweq

--- T20240227/src/main/java/co/yedam/product/service/ProductService.java
+++
@@ -1,9 +0,0 @@
-package co.yedam.product.service;
-
-import java.util.List;
-
-import co.yedam.product.ProductVO;
-
-public interface ProductService {
-	List<ProductVO> productList(ProductVO product);
-}
